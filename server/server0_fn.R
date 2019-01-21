4cc2308dfc10d95b4efbf5661eee2ee1c5212c9f
ewenharrison <mail@ewenharrison.com>
2019-01-21 15:32:16 +0000

    TBS standalone function

--- server/server0_fn.R
+++ server/server0_fn.R
@@ -1,6 +1,264 @@
-group2dummy <- function(length=3, group=1){
-	if(group>length) stop("Group must be an integer less than or equal")
+group2dummy <- function(group, length){
+	if(group>length) stop("Group must be an integer less than or equal to length")
 	out = rep(0, length-1)
 	out[group-1] = 1
 	return(out)
+}
+
+
+# Tranform input variables to desired format
+## Make rdisease_group
+### Cascading ifelses I'm sure could be replaced with something better
+make_rdisease_vec = function(rdisease_primary_tbs, 
+														 rdisease_secondary_tbs,
+														 rdisease_tertiary_tbs,
+														 previous_tx_tbs){
+	out = ifelse(previous_tx_tbs>0, 10,
+							 ifelse(any(
+							 	rdisease_primary_tbs == "1",
+							 	rdisease_secondary_tbs == "1",
+							 	rdisease_tertiary_tbs == "1"), 
+							 	1, 
+							 	ifelse(any(
+							 		rdisease_primary_tbs == "2",
+							 		rdisease_secondary_tbs == "2",
+							 		rdisease_tertiary_tbs == "2"), 
+							 		2, 
+							 		ifelse(any(
+							 			rdisease_primary_tbs == "3",
+							 			rdisease_secondary_tbs == "3",
+							 			rdisease_tertiary_tbs == "3"),
+							 			3, 
+							 			ifelse(any(
+							 				rdisease_primary_tbs == "4",
+							 				rdisease_secondary_tbs == "4",
+							 				rdisease_tertiary_tbs == "4"),
+							 				4,
+							 				ifelse(any(
+							 					rdisease_primary_tbs == "5",
+							 					rdisease_secondary_tbs == "5",
+							 					rdisease_tertiary_tbs == "5"),
+							 					5, 
+							 					ifelse(any(
+							 						rdisease_primary_tbs == "6",
+							 						rdisease_secondary_tbs == "6",
+							 						rdisease_tertiary_tbs == "6"),
+							 						6, 
+							 						ifelse(any(
+							 							rdisease_primary_tbs == "7",
+							 							rdisease_secondary_tbs == "7",
+							 							rdisease_tertiary_tbs == "7"),
+							 							7, 
+							 							ifelse(any(
+							 								rdisease_primary_tbs == "8",
+							 								rdisease_secondary_tbs == "8",
+							 								rdisease_tertiary_tbs == "8"),
+							 								8, 9)
+							 						)
+							 					)
+							 				)
+							 			)
+							 		)
+							 	)
+							 )
+	) %>% 
+		group2dummy(10)
+	out = out[-2]
+	return(out)
+}
+
+## Make rhcv
+is_rhcv = function(rdisease_primary_tbs,
+									 rdisease_secondary_tbs,
+									 rdisease_tertiary_tbs){
+	ifelse(any(
+		rdisease_primary_tbs == 2,
+		rdisease_secondary_tbs == 2,
+		rdisease_tertiary_tbs == 2), 
+		1, 
+		0)
+}
+
+is_cancer = function(rdisease_primary_tbs, 
+										 rdisease_secondary_tbs,
+										 rdisease_tertiary_tbs){
+	ifelse(any(input$rdisease_primary_tbs == 1, 
+						 input$rdisease_secondary_tbs == 1,
+						 input$rdisease_tertiary_tbs == 1),
+				 1,
+				 0)
+}
+
+## Fix this funny Royal Free creatinine thing
+make_rcreatinine = function(centre_tbs, rcreatinine_tbs){
+	if(centre_tbs == "3"){
+		out = (rcreatinine_tbs + 23.4) / 1.2
+	}else{
+		out = rcreatinine_tbs
+	}
+	return(out)
+}
+
+# Make x1 table
+make_x1 = function(rage_tbs,
+									 rgender_tbs,
+									 rhcv,
+									 rdisease_vec,
+									 rcreatinine,
+									 rbilirubin_tbs,
+									 rinr_tbs,
+									 rsodium_tbs,
+									 rpotassium_tbs,
+									 ralbumin_tbs,
+									 rrenal_tbs,
+									 rinpatient_tbs,
+									 rregistration_vec,
+									 rprevious_surgery_tbs,
+									 rencephalopathy_tbs,
+									 rascites_tbs,
+									 rwaiting_time_tbs,
+									 rdiabetes_tbs,
+									 rmax_afp_tbs,
+									 rmax_tumour_size_tbs,
+									 rtumour_number_vec,
+									 dage_tbs,
+									 dcause_vec,
+									 dbmi_tbs,
+									 ddiabetes_vec,
+									 dtype_tbs,
+									 bloodgroup_compatible_tbs,
+									 splittable_tbs){
+	data.frame(raw_x1 = 
+						 	as.numeric(
+						 		c(rage_tbs,
+						 			rage_tbs,
+						 			rgender_tbs,
+						 			rhcv,
+						 			rdisease_vec,
+						 			rcreatinine,
+						 			rbilirubin_tbs,
+						 			rinr_tbs,
+						 			rsodium_tbs,
+						 			rpotassium_tbs,
+						 			ralbumin_tbs,
+						 			rrenal_tbs,
+						 			rinpatient_tbs,
+						 			rregistration_vec,
+						 			rbilirubin_tbs,
+						 			rdisease_vec,
+						 			rage_tbs,
+						 			rprevious_surgery_tbs,
+						 			rencephalopathy_tbs,
+						 			rascites_tbs,
+						 			as.numeric(rwaiting_time_tbs) + 1,
+						 			rdiabetes_tbs,
+						 			rdisease_vec,
+						 			as.numeric(rmax_afp_tbs) + 1,
+						 			rmax_tumour_size_tbs,
+						 			rtumour_number_vec,
+						 			dage_tbs,
+						 			dcause_vec,
+						 			dbmi_tbs,
+						 			ddiabetes_vec,
+						 			dtype_tbs,
+						 			rhcv,
+						 			rhcv,
+						 			rhcv,
+						 			dtype_tbs,
+						 			dtype_tbs,
+						 			rdisease_vec ,
+						 			bloodgroup_compatible_tbs,
+						 			splittable_tbs)
+						 	)
+	)
+}
+
+# Make x2 table
+make_x2 = function(rsodium_tbs,
+									 rbilirubin_tbs,
+									 rcreatinine,
+									 rage_tbs,
+									 ddiabetes_vec,
+									 dage_tbs,
+									 dtype_tbs){
+	data.frame(raw_x2 = 
+						 	as.numeric(
+						 		c(
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			rsodium_tbs,
+						 			rbilirubin_tbs,
+						 			rbilirubin_tbs,
+						 			rbilirubin_tbs,
+						 			rbilirubin_tbs,
+						 			rbilirubin_tbs,
+						 			rbilirubin_tbs,
+						 			rbilirubin_tbs,
+						 			rbilirubin_tbs,
+						 			rcreatinine,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			rage_tbs,
+						 			rage_tbs,
+						 			rage_tbs,
+						 			rage_tbs,
+						 			rage_tbs,
+						 			rage_tbs,
+						 			rage_tbs,
+						 			rage_tbs,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			1,
+						 			ddiabetes_vec,
+						 			dage_tbs,
+						 			rage_tbs,
+						 			rcreatinine,
+						 			dtype_tbs,
+						 			dtype_tbs,
+						 			dtype_tbs,
+						 			dtype_tbs,
+						 			dtype_tbs,
+						 			dtype_tbs,
+						 			dtype_tbs,
+						 			dtype_tbs,
+						 			1,
+						 			1)
+						 	)
+	)
 }
--- server/server1_1.R
+++ server/server1_1.R
@@ -1,229 +1,11 @@
 # TBS calculation
 
 # Define variables 
-## Make rdisease_group
-### Cascading ifelses I'm sure could be replaced with something better
-rdisease_group = reactive({
-	ifelse(input$previous_tx_tbs>0, 10,
-				 ifelse(any(
-				 	input$rdisease_primary_tbs == "1",
-				 	input$rdisease_secondary_tbs == "1",
-				 	input$rdisease_tertiary_tbs == "1"), 
-				 	1, 
-				 	ifelse(any(
-				 		input$rdisease_primary_tbs == "2",
-				 		input$rdisease_secondary_tbs == "2",
-				 		input$rdisease_tertiary_tbs == "2"), 
-				 		2, 
-				 		ifelse(any(
-				 			input$rdisease_primary_tbs == "3",
-				 			input$rdisease_secondary_tbs == "3",
-				 			input$rdisease_tertiary_tbs == "3"),
-				 			3, 
-				 			ifelse(any(
-				 				input$rdisease_primary_tbs == "4",
-				 				input$rdisease_secondary_tbs == "4",
-				 				input$rdisease_tertiary_tbs == "4"),
-				 				4,
-				 				ifelse(any(
-				 					input$rdisease_primary_tbs == "5",
-				 					input$rdisease_secondary_tbs == "5",
-				 					input$rdisease_tertiary_tbs == "5"),
-				 					5, 
-				 					ifelse(any(
-				 						input$rdisease_primary_tbs == "6",
-				 						input$rdisease_secondary_tbs == "6",
-				 						input$rdisease_tertiary_tbs == "6"),
-				 						6, 
-				 						ifelse(any(
-				 							input$rdisease_primary_tbs == "7",
-				 							input$rdisease_secondary_tbs == "7",
-				 							input$rdisease_tertiary_tbs == "7"),
-				 							7, 
-				 							ifelse(any(
-				 								input$rdisease_primary_tbs == "8",
-				 								input$rdisease_secondary_tbs == "8",
-				 								input$rdisease_tertiary_tbs == "8"),
-				 								8, 9)
-				 						)
-				 					)
-				 				)
-				 			)
-				 		)
-				 	)
-				 )
-	)
-})
-
-## Make rhcv
-rhcv = reactive({
-	ifelse(any(
-		input$rdisease_primary_tbs == 2,
-		input$rdisease_secondary_tbs == 2,
-		input$rdisease_tertiary_tbs == 2), 
-		1, 
-		0)
-})
-
-## Make factor with levels>2 vectors
-### This is coded badly in the .xlsx
-### Alcohol==3 actually becomes 1. 
-### Therefore drop the second element of the vector
-rdisease_vec = reactive({
-	out = group2dummy(10, rdisease_group())
-	out = out[-2]
-	return(out)
-})
-rregistration_vec = reactive({
-	group2dummy(7, as.numeric(input$rregistration_tbs))
-})
-
-rtumour_number_vec = reactive({
-	group2dummy(3, as.numeric(input$rtumour_number_tbs))
-})
-
-ddiabetes_vec = reactive({
-	group2dummy(3, as.numeric(input$ddiabetes_tbs))
-})
-
-dcause_vec = reactive({
-	group2dummy(4, as.numeric(input$dcause_tbs))
-})
-
-## Fix this funny Royal Free creatinine thing
-rcreatinine = reactive({
-	if(input$centre_tbs == "3"){
-		out = (input$rcreatinine_tbs + 23.4) / 1.2
-	}else{
-		out = input$rcreatinine_tbs
-	}
-})
-
-# Assemble x1
-x1 = reactive({
-	data.frame(raw_x1 = 
-						 	as.numeric(
-						 		c(input$rage_tbs,
-						 			input$rage_tbs,
-						 			input$rgender_tbs,
-						 			rhcv(),
-						 			rdisease_vec(),
-						 			rcreatinine(),
-						 			input$rbilirubin_tbs,
-						 			input$rinr_tbs,
-						 			input$rsodium_tbs,
-						 			input$rpotassium_tbs,
-						 			input$ralbumin_tbs,
-						 			input$rrenal_tbs,
-						 			input$rinpatient_tbs,
-						 			rregistration_vec(),
-						 			input$rbilirubin_tbs,
-						 			rdisease_vec(),
-						 			input$rage_tbs,
-						 			input$rprevious_surgery_tbs,
-						 			input$rencephalopathy_tbs,
-						 			input$rascites_tbs,
-						 			input$rwaiting_time_tbs+1,
-						 			input$rdiabetes_tbs,
-						 			rdisease_vec(),
-						 			as.numeric(input$rmax_afp_tbs) + 1,
-						 			input$rmax_tumour_size_tbs,
-						 			rtumour_number_vec(),
-						 			input$dage_tbs,
-						 			dcause_vec(),
-						 			input$dbmi_tbs,
-						 			ddiabetes_vec(),
-						 			input$dtype_tbs,
-						 			rhcv(),
-						 			rhcv(),
-						 			rhcv(),
-						 			input$dtype_tbs,
-						 			input$dtype_tbs,
-						 			rdisease_vec (),
-						 			input$bloodgroup_compatible_tbs,
-						 			input$splittable_tbs)))
-})
-
-# Assemble x2 (interactions)
-x2 = reactive({
-	data.frame(raw_x2 = 
-						 	as.numeric(
-						 		c(
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			input$rsodium_tbs,
-						 			input$rbilirubin_tbs,
-						 			input$rbilirubin_tbs,
-						 			input$rbilirubin_tbs,
-						 			input$rbilirubin_tbs,
-						 			input$rbilirubin_tbs,
-						 			input$rbilirubin_tbs,
-						 			input$rbilirubin_tbs,
-						 			input$rbilirubin_tbs,
-						 			rcreatinine(),
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			input$rage_tbs,
-						 			input$rage_tbs,
-						 			input$rage_tbs,
-						 			input$rage_tbs,
-						 			input$rage_tbs,
-						 			input$rage_tbs,
-						 			input$rage_tbs,
-						 			input$rage_tbs,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			1,
-						 			ddiabetes_vec(),
-						 			input$dage_tbs,
-						 			input$rage_tbs,
-						 			rcreatinine(),
-						 			input$dtype_tbs,
-						 			input$dtype_tbs,
-						 			input$dtype_tbs,
-						 			input$dtype_tbs,
-						 			input$dtype_tbs,
-						 			input$dtype_tbs,
-						 			input$dtype_tbs,
-						 			input$dtype_tbs,
-						 			1,
-						 			1)))
+rcreatinine = reactive(make_rcreatinine(input$centre_tbs, input$rcreatinine_tbs))
+rcancer = reactive({
+	is_cancer(input$rdisease_primary_tbs, 
+						input$rdisease_secondary_tbs,
+						input$rdisease_tertiary_tbs)
 })
 
 # Load table of coefficients and survival functions
@@ -231,75 +13,111 @@ betas = readr::read_csv("data/betas.csv")
 surv_cancer = readr::read_csv("data/surv_cancer.csv")
 surv_noncancer = readr::read_csv("data/surv_noncancer.csv")
 
-# Make big prediction table
 linear_prediction = reactive({
-	dplyr::bind_cols(betas, x1(), x2()) %>% 
+	rdisease_vec = make_rdisease_vec(input$rdisease_primary_tbs, 
+																	 input$rdisease_secondary_tbs,
+																	 input$rdisease_tertiary_tbs,
+																	 input$previous_tx_tbs)
+	rhcv = is_rhcv(input$rdisease_primary_tbs,
+								 input$rdisease_secondary_tbs,
+								 input$rdisease_tertiary_tbs)
+	rregistration_vec = group2dummy(as.numeric(input$rregistration_tbs), 7)
+	rtumour_number_vec = group2dummy(as.numeric(input$rtumour_number_tbs), 3)
+	dcause_vec = group2dummy(as.numeric(input$dcause_tbs), 4)
+	ddiabetes_vec = group2dummy(as.numeric(input$ddiabetes_tbs), 3)
+	
+	x1 = make_x1(input$rage_tbs,
+							 input$rgender_tbs,
+							 rhcv,
+							 rdisease_vec,
+							 rcreatinine(),
+							 input$rbilirubin_tbs,
+							 input$rinr_tbs,
+							 input$rsodium_tbs,
+							 input$rpotassium_tbs,
+							 input$ralbumin_tbs,
+							 input$rrenal_tbs,
+							 input$rinpatient_tbs,
+							 rregistration_vec,
+							 input$rprevious_surgery_tbs,
+							 input$rencephalopathy_tbs,
+							 input$rascites_tbs,
+							 input$rwaiting_time_tbs,
+							 input$rdiabetes_tbs,
+							 input$rmax_afp_tbs,
+							 input$rmax_tumour_size_tbs,
+							 rtumour_number_vec,
+							 input$dage_tbs,
+							 dcause_vec,
+							 input$dbmi_tbs,
+							 ddiabetes_vec,
+							 input$dtype_tbs,
+							 input$bloodgroup_compatible_tbs,
+							 input$splittable_tbs)
+	
+	x2 = make_x2(input$rsodium_tbs,
+							 input$rbilirubin_tbs,
+							 rcreatinine(),
+							 input$rage_tbs,
+							 ddiabetes_vec,
+							 input$dage_tbs,
+							 input$dtype_tbs)
+	
+	# Make big prediction table
+	linear_prediction = dplyr::bind_cols(betas, x1, x2) %>% 
 		mutate(transformed_x1 = raw_x1^power,
 					 transformed_x1 = ifelse(ln_1 == 1, log(transformed_x1), transformed_x1),
 					 transformed_x2 = raw_x2^power,
 					 transformed_x2 = ifelse(ln_2 == 1, log(transformed_x2), transformed_x2)
 		)
+	
+	# Select now on cancer or not
+	linear_prediction_table = 
+		if(rcancer() == 1){
+			linear_prediction %>% 
+				dplyr::select(parameter, m1_beta = m1_cancer_beta, m2_beta = m2_cancer_beta, 
+											m1_mean = m1_cancer_mean, m2_mean = m2_cancer_mean, 
+											raw_x1, raw_x2, transformed_x1, transformed_x2) %>% 
+				mutate(m1_x = (transformed_x1*transformed_x2) - m1_mean,
+							 m2_x = (transformed_x1*transformed_x2) - m2_mean,
+							 m1_beta_x = m1_beta * m1_x,
+							 m2_beta_x = m2_beta * m2_x)
+		}else{
+			linear_prediction %>% 
+				dplyr::select(parameter, m1_beta = m1_noncancer_beta, m2_beta = m2_noncancer_beta, 
+											m1_mean = m1_noncancer_mean, m2_mean = m2_noncancer_mean, 
+											raw_x1, raw_x2, transformed_x1, transformed_x2) %>% 
+				mutate(m1_x = (transformed_x1*transformed_x2) - m1_mean,
+							 m2_x = (transformed_x1*transformed_x2) - m2_mean,
+							 m1_beta_x = m1_beta * m1_x,
+							 m2_beta_x = m2_beta * m2_x)
+		}
+	
+	# Sum predictor
+	m1_linear_predictor = sum(linear_prediction_table$m1_beta_x)
+	m2_linear_predictor = sum(linear_prediction_table$m2_beta_x)
+	
+	return(
+		list(m1_linear_predictor=m1_linear_predictor, 
+				 m2_linear_predictor=m2_linear_predictor, 
+				 linear_prediction_table=linear_prediction_table)
+	)
 })
 
-# Select now on cancer or not
-linear_prediction_cancer = reactive({
-	linear_prediction() %>% 
-		dplyr::select(parameter, m1_beta = m1_cancer_beta, m2_beta = m2_cancer_beta, 
-									m1_mean = m1_cancer_mean, m2_mean = m2_cancer_mean, 
-									raw_x1, raw_x2, transformed_x1, transformed_x2) %>% 
-		mutate(m1_x = (transformed_x1*transformed_x2) - m1_mean,
-					 m2_x = (transformed_x1*transformed_x2) - m2_mean,
-					 m1_beta_x = m1_beta * m1_x,
-					 m2_beta_x = m2_beta * m2_x)
-})
-
-linear_prediction_noncancer = reactive({
-	linear_prediction() %>% 
-		dplyr::select(parameter, m1_beta = m1_noncancer_beta, m2_beta = m2_noncancer_beta, 
-									m1_mean = m1_noncancer_mean, m2_mean = m2_noncancer_mean, 
-									raw_x1, raw_x2, transformed_x1, transformed_x2) %>% 
-		mutate(m1_x = (transformed_x1*transformed_x2) - m1_mean,
-					 m2_x = (transformed_x1*transformed_x2) - m2_mean,
-					 m1_beta_x = m1_beta * m1_x,
-					 m2_beta_x = m2_beta * m2_x)
-})
-
-linear_prediction_active = reactive({
-	if(any(input$rdisease_primary_tbs == 1, 
-				 input$rdisease_secondary_tbs == 1,
-				 input$rdisease_tertiary_tbs == 1)){
-		out = linear_prediction_cancer()
-	}else{
-		out = linear_prediction_noncancer()
-	}
-	return(out)
-})
-
+# Pick appropriate survival table
 surv_active = reactive({
-	if(any(input$rdisease_primary_tbs == 1, 
-				 input$rdisease_secondary_tbs == 1,
-				 input$rdisease_tertiary_tbs == 1)){
-		out = surv_cancer
+	if(rcancer() == 1){
+		surv_cancer
 	}else{
-		out = surv_noncancer
+		surv_noncancer
 	}
-	return(out)
 })
-
-# Sum predictor
-m1_linear_predictor = reactive({
-	sum(linear_prediction_active()$m1_beta_x)
-})
-m2_linear_predictor = reactive({
-	sum(linear_prediction_active()$m2_beta_x)
-})
-
 
 m1 = reactive({
-	surv_active()$m1_surv^exp(m1_linear_predictor()) %>% sum()
+	surv_active()$m1_surv^exp(linear_prediction()$m1_linear_predictor) %>% sum()
 })
 m2 = reactive({
-	surv_active()$m2_surv^exp(m2_linear_predictor()) %>% sum()
+	surv_active()$m2_surv^exp(linear_prediction()$m2_linear_predictor) %>% sum()
 })
 tbs = reactive({
 	m2() - m1()
@@ -333,7 +151,7 @@ output$tbsBox <- renderInfoBox({
 
 
 output$x1 = DT::renderDataTable(
-	DT::datatable(linear_prediction_active(),
+	DT::datatable(linear_prediction()$linear_prediction_table,
 								rownames=FALSE, extensions = "FixedColumns",
 								options = list(#dom = 't',
 									scrollX = TRUE,
